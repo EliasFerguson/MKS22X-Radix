538cd57ea907dc3ebf1c9396a76b2ebcfc6188e8
Elias Ferguson <eferguson00@stuy.edu>
2019-04-02 08:59:57 -0400

    testing findMaxLength()

--- Radix.java
+++ Radix.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Radix {
   public static void radixsort(int[] data) {
-    
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    int maxLength = findMaxLength(data);
+  }
+  public static int findMaxLength(int[] data) {
+    int returner = 0;
+    int current = 0;
+    for (int num:data) {
+      current = Math.max(Math.abs(num), current);
+    }
+    while (current != 0) {
+      current /= 10;
+      returner++;
+    }
+    return returner;
   }
 }
