5fe4ee168c8ebc4ea127ce9ec4674f26f1b3d62d
Elias Ferguson <eferguson00@stuy.edu>
2019-04-02 09:03:48 -0400

    findMaxLength works

--- Radix.java
+++ Radix.java
@@ -1,12 +1,17 @@
 import java.util.*;
 
 public class Radix {
+  public static void main(String[] args) {
+    int[] data = {1, 5, 7, 2, 4, 4, 2, 0};
+    System.out.println(findMaxLength(data));
+  }
   public static void radixsort(int[] data) {
     @SuppressWarnings({"unchecked", "rawtypes"})
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     int maxLength = findMaxLength(data);
   }
   public static int findMaxLength(int[] data) {
+    if (data.length == 0) return 1;
     int returner = 0;
     int current = 0;
     for (int num:data) {
